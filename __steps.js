a138bc40bef4161640e296dfad918e0814ecbb6a
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-04-25 03:06:38 +0600

    S

---
+++ __steps.js
@@ -0,0 +1,26 @@
+/*
+1. create a node server with 5 steps
+1.1. create a folder
+1.2. npm init
+1.3. npm i express cors mongodb 
+1.4. script-dev: nodemon index.js
+1.5. crete index.js
+1.6. use 5 steps to create a node server
+
+-----------------
+Create atlas account
+-----------------
+1. signUp google access
+2. create cluster
+3. crete db user and password
+4. network access --> ip address:allow all
+5. database -> connect -> code copy
+
+---------------------
+Crud operation
+---------------------
+
+1. node mongo crud fundamental
+2. create async run function
+
+*/
